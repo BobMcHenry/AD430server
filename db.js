41fc1433e3b443e1c98792ec6fa70615c5515ee5
timdavish <timdavish@gmail.com>
2016-11-07 22:14:44 -0800

    Added comments

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 
+// Module dependencies
 var mysql = require('mysql');
 
+// Databases
 var PRODUCTION_DB = 'ad430_db',
     DEVELOPMENT_DB = 'ad430_db'; // can later be a test db
-
 exports.MODE_PRODUCTION = 'mode_production';
 exports.MODE_DEVELOPMENT = 'mode_development';
 
@@ -12,6 +13,7 @@ var state = {
     mode: null
 };
 
+// Database connect function
 exports.connect = function(mode) {
     state.pool = mysql.createPool({
         host: 'localhost',
@@ -24,6 +26,7 @@ exports.connect = function(mode) {
     state.mode = mode;
 }
 
+// This function returns a database connection
 exports.get = function() {
   return state.pool;
 }
