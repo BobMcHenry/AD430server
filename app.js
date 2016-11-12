e1345c57e6af692d2061aded9f2a8074e193054b
Casey Riggin <krash.kon@gmail.com>
2016-11-12 12:49:09 -0800

    Move End Convo To External File
    Left App.js call in main, - will try to figure out how to move it later.

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var path = require('path'); //Required for pathing to add extra files
  */
 require(path.resolve( __dirname, "./database.js" ))();
 require(path.resolve( __dirname, "./convo_functions/listConvo.js" ))();
+require(path.resolve( __dirname, "./convo_functions/endConvo.js" ))();
 
  
 var app = module.exports = express.createServer();
@@ -100,42 +101,6 @@ app.get('/endConvo', function (req, res) {
 app.listen(80, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
-
-//Update the convo to be over according to the db
-function endConvo(ConvoId, callback)
-{
-	if(ConvoId == undefined)
-	{
-		callback({ "success": false, "message": "ConvoId was not supplied, but is required" });
-		return;
-	}
-
-	//Get and start SQL Connection
-	var con = getConnection();
-	con.connect();
-
-	//Check your input is valid with the DB
-	con.query('SELECT COUNT(*) AS isGood FROM convo WHERE convo_id = ? ', ConvoId , function(err,rows){
-
-		//Check interpreter user id is valid
-		if(rows[0].isGood == 0)
-		{
-			con.end();
-			callback({ "success": false, "message": "Given ConvoId cannot be found." });
-			return;
-		} else {
-			con.query('UPDATE convo SET end_time = NOW() WHERE convo_id = ?', ConvoId, function(err,res){
-			if(err) {
-				callback({ "success": false, "message": "something went wrong in the db." });
-			}
-				console.log(err);
-				console.log(res);
-				callback({ "success": true, "convo_id": ConvoId });
-				return;
-			});
-		}
-	});
-}
 
 
 
