ea6a2c6bb352dd172eedb21fd6c2347294c85628
Bob McHenry <BobMcHenryVide0@gmail.com>
2016-11-14 22:11:51 -0800

    Added updated db files to master repo

--- sql/ad_430_testdata.sql
+++ sql/ad_430_testdata.sql
@@ -1,77 +1,42 @@
 USE ad430_db;
 
 INSERT INTO user(
-<<<<<<< HEAD
-	
-    skype_username, 
-    full_name, 
-<<<<<<< HEAD
-    
-    
-    
-=======
-
-    microsft_api_info,
-    full_name,
-
-
-
->>>>>>> origin/AA_SetPrefs
-=======
->>>>>>> master
+    skype_username,
+	full_name,
+    email,
     is_interpreter,
-    ok_to_chat,
+    ok_to_chat, 
     ok_to_show_location,
-<<<<<<< HEAD
     last_known_location_lat, 
     last_known_location_long,
     hashed_password
 )VALUES
- ("skypeUN01", "Seattle PersonA", 0, 1, 1, 47.6080, -122.3351, "invalidpasswordhashinvalidpasswordhashinvalidpasswordhashinvalidpasswordhash"),
- ("skypeUN02", "Seattle PersonB", 0, 1, 1, 47.6080, -122.3351, "invalidpasswordhashinvalidpasswordhashinvalidpasswordhashinvalidpasswordhash"),
- ("skypeUN03", "Seattle PersonC", 1, 0, 1, 47.6080, -122.3351, "invalidpasswordhashinvalidpasswordhashinvalidpasswordhashinvalidpasswordhash"),
- ("skypeUN04", "Seattle PersonD", 1, 0, 1, 47.6080, -122.3351, "invalidpasswordhashinvalidpasswordhashinvalidpasswordhashinvalidpasswordhash"),
+ ("skypeUN01", "Casey D Riggin", "fakeemail01@gmail.com", 0, 1, 1, 47.6080, -122.3351, "LNBrovtzqtePOC1nnj919bc3e7fe7558b830c5c910ca93c0b77a161f940b"),
+ ("skypeUN02", NULL, "fakeemail02@gmail.com", 0, 1, 1, 47.6080, -122.3351, "LNBrovtzqtePOC1nnj919bc3e7fe7558b830c5c910ca93c0b77a161f940b"),
+ ("skypeUN03", "Lizzy Sizer", "fakeemail03@gmail.com", 1, 0, 1, 47.6080, -122.3351, "LNBrovtzqtePOC1nnj919bc3e7fe7558b830c5c910ca93c0b77a161f940b"),
+ ("skypeUN04", NULL, "fakeemail04@gmail.com", 1, 0, 1, 47.6080, -122.3351, "LNBrovtzqtePOC1nnj919bc3e7fe7558b830c5c910ca93c0b77a161f940b"),
  
- ("skypeUN05", "Tacoma PersonA", 0, 1, 0, 47.2528, -122.4442, "invalidpasswordhashinvalidpasswordhashinvalidpasswordhashinvalidpasswordhash"),
- ("skypeUN06", "Tacoma PersonB", 0, 1, 0, 47.2528, -122.4442, "invalidpasswordhashinvalidpasswordhashinvalidpasswordhashinvalidpasswordhash"),
- ("skypeUN07", "Tacoma PersonC", 1, 0, 0, 47.2528, -122.4442, "invalidpasswordhashinvalidpasswordhashinvalidpasswordhashinvalidpasswordhash"),
- ("skypeUN08", "Tacoma PersonD", 1, 0, 0, 47.2528, -122.4442, "invalidpasswordhashinvalidpasswordhashinvalidpasswordhashinvalidpasswordhash"),
+ ("skypeUN05", "Maria Alburta", "fakeemail05@gmail.com", 0, 1, 0, 47.2528, -122.4442, "LNBrovtzqtePOC1nnj919bc3e7fe7558b830c5c910ca93c0b77a161f940b"),
+ ("skypeUN06", "Douglas Riggin", "fakeemail06@gmail.com", 0, 1, 0, 47.2528, -122.4442, "LNBrovtzqtePOC1nnj919bc3e7fe7558b830c5c910ca93c0b77a161f940b"),
+ ("skypeUN07", NULL, "fakeemail07@gmail.com", 1, 0, 0, 47.2528, -122.4442, "LNBrovtzqtePOC1nnj919bc3e7fe7558b830c5c910ca93c0b77a161f940b"),
+ ("skypeUN08", NULL,"fakeemail08@gmail.com", 1, 0, 0, 47.2528, -122.4442, "LNBrovtzqtePOC1nnj919bc3e7fe7558b830c5c910ca93c0b77a161f940b"),
  
- ("skypeUN09", "Bellevue PersonA", 0, 1, 1, 47.6080, -122.3351, "invalidpasswordhashinvalidpasswordhashinvalidpasswordhashinvalidpasswordhash"),
- ("skypeUN10", "Bellevue PersonB", 0, 1, 1, 47.6080, -122.3351, "invalidpasswordhashinvalidpasswordhashinvalidpasswordhashinvalidpasswordhash"),
- ("skypeUN11", "Bellevue PersonC", 1, 1, 1, 47.6080, -122.3351, "invalidpasswordhashinvalidpasswordhashinvalidpasswordhashinvalidpasswordhash"),
- ("skypeUN12", "Bellevue PersonD", 1, 1, 1, 47.6080, -122.3351, "invalidpasswordhashinvalidpasswordhashinvalidpasswordhashinvalidpasswordhash")
-=======
-    last_known_location_lat,
-    last_known_location_long
-)VALUES
- ("microAccts01", "Seattle PersonA", 0, 1, 1, 47.6080, -122.3351),
- ("microAccts02", "Seattle PersonB", 0, 1, 1, 47.6080, -122.3351),
- ("microAccts03", "Seattle PersonC", 1, 0, 1, 47.6080, -122.3351),
- ("microAccts04", "Seattle PersonD", 1, 0, 1, 47.6080, -122.3351),
-
- ("microAccts05", "Tacoma PersonA", 0, 1, 0, 47.2528, -122.4442),
- ("microAccts06", "Tacoma PersonB", 0, 1, 0, 47.2528, -122.4442),
- ("microAccts07", "Tacoma PersonC", 1, 0, 0, 47.2528, -122.4442),
- ("microAccts08", "Tacoma PersonD", 1, 0, 0, 47.2528, -122.4442),
-
- ("microAccts09", "Bellevue PersonA", 0, 1, 1, 47.6080, -122.3351),
- ("microAccts10", "Bellevue PersonB", 0, 1, 1, 47.6080, -122.3351),
- ("microAccts11", "Bellevue PersonC", 1, 1, 1, 47.6080, -122.3351),
- ("microAccts12", "Bellevue PersonD", 1, 1, 1, 47.6080, -122.3351)
->>>>>>> origin/AA_SetPrefs
+ ("skypeUN09", NULL, "fakeemail09@gmail.com", 0, 1, 1, 47.6080, -122.3351, "LNBrovtzqtePOC1nnj919bc3e7fe7558b830c5c910ca93c0b77a161f940b"),
+ ("skypeUN10", NULL, "fakeemail10@gmail.com", 0, 1, 1, 47.6080, -122.3351, "LNBrovtzqtePOC1nnj919bc3e7fe7558b830c5c910ca93c0b77a161f940b"),
+ ("skypeUN11", NULL, "fakeemail11@gmail.com", 1, 1, 1, 47.6080, -122.3351, "LNBrovtzqtePOC1nnj919bc3e7fe7558b830c5c910ca93c0b77a161f940b"),
+ ("skypeUN12", "Fake Full Name", "fakeemail12@gmail.com", 1, 1, 1, 47.6080, -122.3351, "LNBrovtzqtePOC1nnj919bc3e7fe7558b830c5c910ca93c0b77a161f940b")
 ;
 
 INSERT INTO user_report(
-
+	
     creating_user_id, #not null
     blocking_user_id,#not null
-
+    
     creation_timestamp,
-
+    
     was_reported,#not null
     reporting_user_comment
-
+    
 ) VALUES
 ( 1, 2, NOW(), 0, "comments"),
 ( 1, 3, NOW(), 1, "comments"),
@@ -82,15 +47,15 @@ INSERT INTO user_report(
 ;
 
 INSERT INTO convo(
-
-
+	
+   
     hoh_user_id,
     start_time,
     interpreter_user_id,
     last_updated_hoh,
     last_updated_interpreter
-
-
+    
+    
 ) VALUES
 ( 1, NOW(), 2, NOW(), NOW()  ),
 ( 1, NOW(), 3, NOW(), NOW() ),
@@ -100,11 +65,11 @@ INSERT INTO convo(
 ( 9, NOW(), 11, NOW(), NOW()  )
 
 ;INSERT INTO convo_rating(
-	convo_id,
-    asl_skill,
+	convo_id, 
+    asl_skill, 
     translate_speed,
-    friendliness
-
+    friendliness 
+    
 ) VALUES
 (1, 5, 5, 5)
 
