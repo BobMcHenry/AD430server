60ea55e4e43bd932b1dec73d80be50e4232ee252
GrantZukowski <grantzukowski@gmail.com>
2016-12-05 18:35:22 -0800

    adding first test case and dependent Junit libraries, currently testing live server, will run on test server as well

---
+++ src/GetVideoStatus.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import org.junit.Test;
+
+public class GetVideoStatus {
+	
+	@Test
+	public void test() throws IOException {
+		
+		URL myURL = new URL("http://54.69.18.19/getLocationStatus?userId=1");
+		BufferedReader in = new BufferedReader(new InputStreamReader(myURL.openStream()));
+		
+		String inputLine = in.readLine();
+		in.close();
+		assertEquals("Not yet implemented", inputLine, "{\"ok_to_show_location\":true}");
+		
+		System.out.println(inputLine);
+	}	
+}
