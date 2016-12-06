62cf7d461a0378fa2708e9505ff0ae08daeb0b7a
GrantZukowski <grantzukowski@gmail.com>
2016-12-05 19:10:24 -0800

    added 4 unit tests corresponding with 4 routes

--- src/SetVideoStatus.java
+++ src/SetVideoStatus.java
@@ -5,16 +5,19 @@ import java.io.InputStreamReader;
 import java.net.URL;
 
 import org.junit.Test;
-import org.omg.CORBA.SystemException;
 
-public class GetVideoStatus {
+public class SetVideoStatus {
 	
 	@Test
 	public void test() throws IOException {
-		URL myURL = new URL("http://54.69.18.19/getVideoStatus?userId=1");
+		
+		URL myURL = new URL("http://54.69.18.19/setVideoStatus?userId=1&status=1");
 		BufferedReader in = new BufferedReader(new InputStreamReader(myURL.openStream()));
+		
 		String inputLine = in.readLine();
 		in.close();
-		assertEquals("Not yet implemented", inputLine, "{\"ok_to_chat\":true}");
+	
+		assertEquals("Not yet implemented", inputLine, "{\"success\":true,\"ok_to_chat\":\"1\"}");
+		
 	}	
 }
